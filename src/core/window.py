bee6a558afe78bcb0b93962d934475d588f4d61f
Onuelito <90167681+Nuelito@users.noreply.github.com>
2022-10-09 21:43:15 -0400

    removed window class

--- src/core/window.py
+++ src/core/window.py
@@ -5,11 +5,11 @@ window the main things will be done inside
 to other scripts
 """
 
-import time
 import pyglet
 from pyglet.gl import*
 from pyglet.window import key
 
+import src.config.objects as objects
 import src.core.canvas as canvas
 import src.core.dhist as dhist
 import src.core.mouse as mouse
@@ -24,146 +24,146 @@ RED     = 0xFF0000FF
 GREEN   = 0xFF00FF00
 BLUE    = 0xFFFF0000
 
-MODES   = ["Draw", "Fill", "Eraser"]
-BGCOLOR = [.7, .7, .7 , 1]
+MODES = ["Draw", "Fill", "Eraser"]
 KEYBOARD = key.KeyStateHandler()
+BGCOLOR = [.7, .7, .7 , 1]
 
-class Window(pyglet.window.Window):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-       
-        self.color = BLACK
-        self.brush_size = 0
-        self.mode = MODES[0]
-        #Initializing canvas
-        canvas.x, canvas.y = self.width//2, self.height//2
-        canvas.set_size(640, 480); canvas.data.fill(WHITE)
-            
-        #Initializing draw history module
-        dhist.data[dhist.index] = np.copy(canvas.data)
-        dhist.index = 0
+#Window
+window = pyglet.window.Window(800, 600, caption="Hello Pyglet")
+brush_size = 0
+mode = MODES[0]
+color = BLACK
+
+
+#Initializing canvas
+canvas.x, canvas.y = window.width//2, window.height//2
+canvas.set_size(640, 480); canvas.data.fill(WHITE)
+
+#Initializing draw history module
+dhist.data[dhist.index] = np.copy(canvas.data)
+dhist.index = 0
+
+def save():
+    save_img = canvas.data.tobytes()
+    save_img = pyglet.image.ImageData(canvas.width, canvas.height, 'RGBA', save_img)
+
+    save_img.save("Untitled.png")
+    print("[Image saved as \"Untitled.png\" in current directory]")
+
+def draw(x, y, mx, my):
+    dcolor = color
+    if mode == MODES[2]: dcolor = WHITE
+    clib.draw(dcolor, brush_size, x, y, mx, my)
+
+def flood_fill(x, y): clib.flood_fill(color, x, y); dhist.update()
+
+def tool_mode(index):
+    global mode
+    print("Set mode: "+MODES[index])
+    mode = MODES[index]
+
+def set_color(ncolor):
+    global color
+    color = ncolor
+    print("Color set to: ", end="")
+    if color == BLACK: print("Black")
+    if color == WHITE: print("White")
+    if color == RED: print("Red")
+    if color == GREEN: print("Green")
+    if color == BLUE: print("Blue")
+
+def zoom(x, y, scroll_y):
+    old_width, old_height = canvas.width*canvas.zoom, canvas.height*canvas.zoom
+    canvas.zoom = min(max(canvas.zoom+scroll_y/10, canvas.min_zoom), canvas.max_zoom)
         
-        self.push_handlers(KEYBOARD)
-        pyglet.clock.schedule_interval(self.update, 1/60)
+    #Moving canvas
+    new_width, new_height = canvas.width*canvas.zoom, canvas.height*canvas.zoom
+    per_width, per_height = new_width/old_width, new_height/old_height #Percentage
+    delta_x, delta_y      = (x-canvas.x)*per_width, (y-canvas.y)*per_height
+    canvas.x, canvas.y    = x-delta_x, y-delta_y
 
-    def save(self):
-       save_img = canvas.data.tobytes()
-       save_img = pyglet.image.ImageData(canvas.width, canvas.height, 'RGBA', save_img)
+def set_brush_size(size): 
+    global brush_size
+    brush_size = min(max(0, size), 50)
+    print("Brush size is not: %d"%(brush_size))
 
-       save_img.save("Untitled.png")
-       print("[Image saved as \"Untitled.png\" in current directory]")
+#MOUSE EVENTS#
+@window.event
+def on_mouse_press(x, y, bttn, mod):
+    if bttn == 1 and mode == MODES[1]:
+        flood_fill(x, y)
+    mouse.x, mouse.y = x, y
 
-    def zoom(self, x, y, scroll_y):
+@window.event
+def on_mouse_release(x, y, bttn, mod):
+    if (mode == MODES[0] or mode == MODES[2]) and bttn == 1: dhist.update()
+
+@window.event
+def on_mouse_drag(x, y, dx, dy, bttn, mod):
+    if (mode == MODES[0] or mode == MODES[2]) and bttn == 1:
+        draw(x, y, mouse.x, mouse.y)
+
+    mouse.x, mouse.y = x, y
+    if bttn == 2: canvas.x += dx; canvas.y += dy
+
+@window.event
+def on_mouse_scroll(x, y, scroll_x, scroll_y):
+    if KEYBOARD[key.LSHIFT]: 
+        if scroll_y > 0: set_brush_size(brush_size+1)
+        else: set_brush_size(brush_size - 1)
+    
+    else: zoom(x, y, scroll_y)
+
+#KEYBOARD EVENTS#
+@window.event
+def on_key_press(symbol, mod):
+    """
+    Function for key shortcuts
+    """
+    if mod & key.MOD_CTRL:
+        if symbol == key.S: save()
+        if symbol == key.Z: 
+            if KEYBOARD[key.LSHIFT]: dhist.redo()
+            else: dhist.undo()
+
+        if symbol == key._1: set_color(BLACK)
+        if symbol == key._2: set_color(WHITE)
+        if symbol == key._3: set_color(RED)
+        if symbol == key._4: set_color(GREEN)
+        if symbol == key._5: set_color(BLUE)
+
+    #Reset canvas
+    if symbol == key.R:
+        canvas.zoom = 1
+        canvas.x = window.width//2
+        canvas.y = window.height//2
+
+    if symbol == key.B: tool_mode(0)
+    if symbol == key.F: tool_mode(1)
+    if symbol == key.C: canvas.data.fill(WHITE); dhist.update()
+    if symbol == key.E: tool_mode(2)
+
+@window.event
+def on_draw():
+    output = canvas.data.tobytes()
+    output = pyglet.image.ImageData(canvas.width, canvas.height, 'RGBA', output)
+    output = output.get_texture()
+
+    glClearColor(*BGCOLOR)
+
+    if canvas.zoom > 1:
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
         
-        #Recording old dimensions and modifying zoom
-        old_width, old_height = canvas.width*canvas.zoom, canvas.height*canvas.zoom
-        canvas.zoom = min(max(canvas.zoom+scroll_y/10, canvas.min_zoom), canvas.max_zoom)
-        
-        #Moving canvas
-        new_width, new_height = canvas.width*canvas.zoom, canvas.height*canvas.zoom
-        per_width, per_height = new_width/old_width, new_height/old_height #Percentage
-        delta_x, delta_y      = (x-canvas.x)*per_width, (y-canvas.y)*per_height
-        canvas.x, canvas.y    = x-delta_x, y-delta_y
+    window.clear()
+    output.width = int(canvas.width*canvas.zoom)
+    output.height = int(canvas.height*canvas.zoom)
+    output.anchor_x = output.width//2
+    output.anchor_y = output.height//2
+    output.blit(canvas.x, canvas.y)
 
-    def set_color(self, color):
-        self.color = color
 
-        print("Color set to: ", end="")
-        if color == BLACK: print("Black")
-        if color == WHITE: print("White")
-        if color == RED: print("Red")
-        if color == GREEN: print("Green")
-        if color == BLUE: print("Blue")
+def update(dt): pass
 
-    def set_thickness(self, size):
-        self.brush_size = min(max(0, size), 50)
-        print("Brush size is now: %d"%(self.brush_size))
-
-    def flood_fill(self, x, y): 
-        #start = time.perf_counter()
-        clib.flood_fill(self.color, x, y)
-        dhist.update()
-        #end = time.perf_counter()
-        #print("Filled in %.5f seconds"%(end-start))
-
-    def draw(self, x, y, mx, my): 
-        color = self.color
-        if self.mode == MODES[2]: color = WHITE
-        clib.draw(color, self.brush_size, x, y, mx, my)
-
-    def tool_mode(self, mode):
-        print("Set mode: "+MODES[mode])
-        self.mode = MODES[mode]
-
-    def on_key_press(self, symbol, mod):
-        """
-        This function will hold the special keys for
-        changing tool mode(i.e: 1 = Pencil, 2 = Bucket).
-        """
-        if mod & key.MOD_CTRL:
-            if symbol == key.S: self.save()
-            if symbol == key.Z: dhist.undo()
-            if symbol == key.Y: dhist.redo()
-
-            if symbol == key._1: self.set_color(BLACK)
-            if symbol == key._2: self.set_color(WHITE)
-            if symbol == key._3: self.set_color(RED)
-            if symbol == key._4: self.set_color(GREEN)
-            if symbol == key._5: self.set_color(BLUE)
-
-        if symbol == key.B: self.tool_mode(0)
-        if symbol == key.F: self.tool_mode(1)
-        if symbol == key.C: canvas.data.fill(WHITE); dhist.update()
-        if symbol == key.E: self.tool_mode(2)
-        if symbol == key.R:
-            canvas.zoom = 1
-            canvas.x = self.width//2
-            canvas.y = self.height//2
-
-    def on_mouse_press(self, x, y, bttn, mod): 
-
-        #Fill Mode functions
-        if self.mode == MODES[1]: 
-            if bttn == 1: self.flood_fill(x, y)
-
-        mouse.x, mouse.y = x, y
-
-    def on_mouse_release(self, x, y, bttn, mod):
-        if self.mode == MODES[0] or self.mode == MODES[2]: dhist.update()
-
-    def on_mouse_drag(self, x, y, dx, dy, bttn, mod):
-        
-        if (self.mode == MODES[0] or self.mode == MODES[2]) and bttn == 1:
-            self.draw(x, y, mouse.x, mouse.y)
-
-        if bttn == 2: canvas.x += dx; canvas.y += dy
-        mouse.x, mouse.y = x, y
-
-    def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
-        if not KEYBOARD[key.LSHIFT]: self.zoom(x, y, scroll_y)
-        else:
-            if scroll_y > 0: self.set_thickness(self.brush_size + 1)
-            else: self.set_thickness(self.brush_size - 1)
-
-    def on_draw(self):
-        output = canvas.data.tobytes()
-        output = pyglet.image.ImageData(canvas.width, canvas.height, 'RGBA', output)
-        output = output.get_texture()
-
-        glClearColor(*BGCOLOR)
-
-        if canvas.zoom > 1:
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        
-        self.clear()
-        output.width = int(canvas.width*canvas.zoom)
-        output.height = int(canvas.height*canvas.zoom)
-        output.anchor_x = output.width//2
-        output.anchor_y = output.height//2
-        output.blit(canvas.x, canvas.y)
-
-        gui.draw()
-
-    def update(self, dt):
-        gui.update()
+window.push_handlers(KEYBOARD)
+pyglet.clock.schedule_interval(update, 1/60)

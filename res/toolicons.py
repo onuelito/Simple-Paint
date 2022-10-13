b6e7e4ed78208a0faa4e08670b9bf25e2fed638f
Onuelito <90167681+Nuelito@users.noreply.github.com>
2022-10-12 23:38:36 -0400

    icons

---
+++ res/toolicons.py
@@ -0,0 +1,7 @@
+import pyglet
+
+PATH   = "res/tools_icons/"
+PENCIL = pyglet.resource.image(PATH+"pencil.png")
+ERASER = pyglet.resource.image(PATH+"eraser.png")
+BUCKET = pyglet.resource.image(PATH+"bucket.png")
+COLORS = pyglet.resource.image(PATH+"colors.png")

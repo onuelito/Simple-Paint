67b801d2070b643797c00c301f38854c6846e4a8
onuelito <90167681+onuelito@users.noreply.github.com>
2023-02-25 18:47:52 -0500

    Bye

--- res/systmicons.py
+++
@@ -1,4 +0,0 @@
-import pyglet
-
-PATH   = "res/systm_icons/"
-SAVEDK = pyglet.resource.image(PATH+"floppy.png")
--- res/toolicons.py
+++
@@ -1,7 +0,0 @@
-import pyglet
-
-PATH   = "res/tools_icons/"
-PENCIL = pyglet.resource.image(PATH+"pencil.png")
-ERASER = pyglet.resource.image(PATH+"eraser.png")
-BUCKET = pyglet.resource.image(PATH+"bucket.png")
-COLORS = pyglet.resource.image(PATH+"colors.png")
